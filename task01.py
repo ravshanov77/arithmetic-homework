0ea5e709dae6e88d533155d8a651e842d0c176f4
FarruxRavshanov <farruxlynx@gmail.com>
2023-01-29 07:38:45 +0300

    01

--- task01.py
+++ task01.py
@@ -1,8 +1,15 @@
-odd=1111
-x1 = odd % 10
-x2 = odd // 10 % 10
-x3 = odd // 100 % 10
-x4 = odd // 1000
+def sum_odd(num: int):
+    x1 = num % 10
+    x2 = num // 10 % 10
+    x3 = num // 100 % 10
+    x4 = num // 1000
 
-n = x1 % 2 + x2 % 2 + x3 % 2 + x4 % 2
-print(n)
+    sum = 0
+    sum += (x1 % 2) * x1
+    sum += (x2 % 2) * x2
+    sum += (x3 % 2) * x3
+    sum += (x4 % 2) * x4
+    return sum
+
+
+print(sum_odd(2347))
